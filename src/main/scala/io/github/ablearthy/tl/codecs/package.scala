de38ab43278ed1bfb2a5f12babcf8df4dde14e5b
Able Arthy <ablearthy@gmail.com>
2023-06-07 16:35:21 +0300

    Add types to implicits

--- src/main/scala/io/github/ablearthy/tl/codecs/package.scala
+++ src/main/scala/io/github/ablearthy/tl/codecs/package.scala
@@ -1,6 +1,9 @@
 package io.github.ablearthy.tl
 
+import io.github.ablearthy.tl.aliases.Bytes
+import io.circe.{Decoder, Encoder}
+
 package object codecs {
-  implicit final val bytesDecoder = BytesCodecs.bytesDecoder
-  implicit final val bytesEncoder = BytesCodecs.bytesEncoder
+  implicit final val bytesDecoder: Decoder[Bytes] = BytesCodecs.bytesDecoder
+  implicit final val bytesEncoder: Encoder[Bytes] = BytesCodecs.bytesEncoder
 }
